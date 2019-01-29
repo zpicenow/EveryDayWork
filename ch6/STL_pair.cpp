2e957777786b85e7e17d378c56f4515047126e39
zhaopeng <zp2016@mail.dlut.edu.cn>
2019-01-29 16:51:24 +0800

    2019年01月29日打卡

---
+++ ch6/STL_pair.cpp
@@ -0,0 +1,20 @@
+//
+// Created by zhaopeng on 19-1-29.
+//
+
+/*
+ * pair基本操作
+ */
+
+#include <stdio.h>
+#include <utility>
+#include <iostream>
+
+using namespace std;
+int main() {
+    pair<string, int> p;
+    p = make_pair("hhhhh", 121312);
+    p.first = "ferge";
+    p.second = 334342;
+    p = pair<string, int>("jkjjj", 345);
+}
--- ch6/STL_vector.cpp
+++ ch6/STL_vector.cpp
@@ -1,12 +1,39 @@
 //
 // Created by zhaopeng on 19-1-28.
 //
-
+/*
+ * 杂项
+ *
+ */
 #include <stdio.h>
 #include <vector>
-
+#include <string>
+#include <queue>
+#include <map>
 using namespace std;
 int main() {
     vector<int> vi;
-    vi.erase()
+    map<string,int> mp;
+
+    mp.insert(make_pair("hhh", 342));
 }
+
+struct fruit {
+    string name;
+    int price;
+
+    friend bool operator<(fruit f1, fruit f2) {
+        return f1.price < f2.price;
+    }
+
+};
+
+struct cmp {
+    bool  operator()(fruit f1,fruit f2) {
+        return f1.price > f2.price;
+    }
+};
+
+//此时定义方式必须是第三个参数写cmp
+priority_queue<fruit, vector<fruit>, cmp> q;
+
