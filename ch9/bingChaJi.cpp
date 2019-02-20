f793ab75111e06e93147e548c0e635cc8dd07faf
zhaopeng <zp2016@mail.dlut.edu.cn>
2019-02-20 13:49:55 +0800

    2019年02月20日

--- ch9/bingChaJi.cpp
+++ ch9/bingChaJi.cpp
@@ -8,7 +8,7 @@
 
 #include <stdio.h>
 int father[10010] = {0};
-int findFather(int x) {
+int findFather1(int x) {
     while (x != father[x]) {
         x = father[x];
     }
@@ -16,9 +16,36 @@ int findFather(int x) {
 }
 
 void Union(int a,int b) {
-    int faA = findFather(a);
-    int faB = findFather(b);
+    int faA = findFather1(a);
+    int faB = findFather1(b);
     if (faA != faB) {
         father[faA] = faB;
     }
+}
+
+//int findFather(int x) {
+//    int a = x;
+//    while (x != father[x]) {
+//        x = father[x];
+//    }
+//    while (a != father[a]) {
+//        int z = a;
+//        a = father[a];
+//        father[z] = x;
+//    }
+//    return x;
+//
+//}
+
+//递归
+int findFather(int x) {
+    if (x == father[x]) {
+        return x;
+    } else {
+        int F = findFather(father[x]);
+        father[x] = F;
+        return F;
+
+    }
+
 }
